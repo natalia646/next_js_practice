948d913cecfc02daa579056d0f182d70eca9b65f
natalia646 <75090544+natalia646@users.noreply.github.com>
2024-04-03 18:36:52 +0300

    test img

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,4 +1,6 @@
 import { useTranslations } from "next-intl";
+import Image from "next/image";
+import photo from "/public/namephoto.png"
 
 export default function Index() {
   const t = useTranslations("Index");
@@ -6,7 +8,8 @@ export default function Index() {
     <>
       <h1>{t("title")}</h1>
       <h2>{t("desc")}</h2>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat corporis voluptates pariatur dolorem saepe reprehenderit incidunt atque? Quaerat officia adipisci dicta blanditiis iure, voluptatum vel sit, sapiente quas ea laboriosam.</p>
+      <Image src={photo} alt="photo" priority></Image>
+      <p >Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat corporis voluptates pariatur dolorem saepe reprehenderit incidunt atque? Quaerat officia adipisci dicta blanditiis iure, voluptatum vel sit, sapiente quas ea laboriosam.</p>
     </>
   );
 }
