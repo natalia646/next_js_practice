b133d44ca6fb18d55b13b613468e36177d9fd068
natalia646 <75090544+natalia646@users.noreply.github.com>
2024-04-19 08:20:10 +0300

    message

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -8,7 +8,7 @@ export default function Index() {
     <>
       <h1>{t("title")}</h1>
       <h2>{t("desc")}</h2>
-      <Image src={photo} alt="photo" priority></Image>
+      <Image src={photo} alt="photo" ></Image>
       <p >Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat corporis voluptates pariatur dolorem saepe reprehenderit incidunt atque? Quaerat officia adipisci dicta blanditiis iure, voluptatum vel sit, sapiente quas ea laboriosam.</p>
     </>
   );
